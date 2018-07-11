991036d24f3292d252f1b0a7beae607c03513583
jenn14108 <jenn.lee14108@gmail.com>
2018-07-11 13:03:48 -0400

    Add transloc key as environmental variable

--- TestTransLocAPI.js
+++ TestTransLocAPI.js
@@ -8,8 +8,9 @@ Below is an example with Harvard
 
 // This code uses GET Agencies (with geo area filter) to obtain agency numbers in the MA area
 //Harvard agency number = 52
+const transLocAPIKey = process.env.TRANSLOC_KEY || 'mongodb://localhost/DeisTransportApp'
 unirest.get("https://transloc-api-1-2.p.mashape.com/agencies.json?callback=call&geo_area=42.445496%2C+-71.000767%7C42.233368%2C+-71.462164")
-.header("X-Mashape-Key", "F8LjbwI0cZmsh6WYqljd7CAvik0Yp1IB1MijsnMwhG4ckNi4m1")
+.header("X-Mashape-Key", transLocAPIKey)
 .header("Accept", "application/json")
 .end(function (result) {
   console.log(result.status, result.headers, result.body);
@@ -19,7 +20,7 @@ unirest.get("https://transloc-api-1-2.p.mashape.com/agencies.json?callback=call&
 // This code uses GET Stops (with Harvard's agency number) to obtain a list of ALL stops for all routes
 //**** It also gives us the stop_id of particular stops
 unirest.get("https://transloc-api-1-2.p.mashape.com/stops.json?agencies=52&callback=call")
-.header("X-Mashape-Key", "F8LjbwI0cZmsh6WYqljd7CAvik0Yp1IB1MijsnMwhG4ckNi4m1")
+.header("X-Mashape-Key", transLocAPIKey)
 .header("Accept", "application/json")
 .end(function (result) {
   //console.log(result.status, result.headers, result.body);
@@ -34,7 +35,7 @@ unirest.get("https://transloc-api-1-2.p.mashape.com/stops.json?agencies=52&callb
 
 // This code obtains the arrival times of agency 12 (Go Triangle)
 unirest.get("https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=12&callback=call")
-.header("X-Mashape-Key", "F8LjbwI0cZmsh6WYqljd7CAvik0Yp1IB1MijsnMwhG4ckNi4m1")
+.header("X-Mashape-Key", transLocAPIKey)
 .header("Accept", "application/json")
 .end(function (result) {
   //console.log(result.status, result.headers, result.body);
