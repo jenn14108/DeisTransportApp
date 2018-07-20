280614c9556297511aed27f377cac822b39761b9
jenn14108 <jenn.lee14108@gmail.com>
2018-07-20 10:46:39 -0400

    Add method for schedule retrieval

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -6,5 +6,13 @@ exports.renderMain = (req,res) => {
 };
 
 exports.getSchedule = (req,res) => {
-  Query.getTimesForStop()
+  console.log("in getSchedules")
+  var route = req.body.schedule
+  console.log("fetching schedule of route: " + route)
+  var stops = Query.getSchedule(route).then(response => console.log(response)).catch(err => console.log("err2: "+err))
+  var times = []
+  for (var i = 0; i < stops.length; i++) {
+    times[i] = Query.getTimesForStop(route, stops[i]).then(response => console.log(response)).catch(err => console.log("err2: "+err))
+  }
+  console.log(times)
 }
