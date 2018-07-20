b87f066acf0356d4e5f6cf120917bd3b4b1dcc34
Ek Kheng Chung <chungek@brandeis.edu>
2018-07-20 01:51:49 -0400

    schedules disp still broken

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -10,11 +10,11 @@ exports.getSchedule = (req,res) => {
   console.log("in getSchedules")
   var route = req.body.schedule
   console.log("fetching schedule of route: " + route)
-  var stops = Query.getSchedule(route).then(response => console.log(response)).catch(err => console.log("err2: "+err))
-  var times = []
-  for (var i = 0; i < stops.length; i++) {
-    times[i] = Query.getTimesForStop(route, stops[i]).then(response => console.log(response)).catch(err => console.log("err2: "+err))
-  }
-  console.log(times)
-  res.render("schedules", {stops:stops, times:times});
+  Query.getSchedule(route).then(response => console.log(response)).catch(err => console.log("err2: "+err))
+  // for (var i = 0; i < stops.length; i++) {
+  //   times[i] = Query.getTimesForStop(route, stops[i]).then(response => console.log(response)).catch(err => console.log("err2: "+err))
+  // }
+  //console.log(times)
+  // console.log(stops)
+  //res.render("schedules", {stops:stops});
 }
