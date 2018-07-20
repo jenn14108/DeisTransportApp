ed4eef628abf9e7663c97d2265940775e21d985e
Casper L'Esperance-Kerckhoff <casperlesperancekerckhoff@gmail.com>
2018-07-19 21:39:47 -0400

    fixed time change bug that would cause VanDays that occur after the end of DST not to be found

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const
  EnterSchedule = require('./EnterSchedule')
  Query = require('./Query')
  transloc_key = process.env.TRANSLOC_KEY;
+ var async = require('async')
 
 console.log('API server listening...');
 
@@ -53,9 +54,12 @@ db.once('open', function() {
 
 // console.log(Query.getSchedule(1010))
 // Query.getSchedule(1010).then(response => console.log(response)).catch(err => console.log("err2: "+err))
-Query.getTimesForStop(1010, "Rabb").then(response => console.log(response)).catch(err => console.log("err2: "+err))
+// Query.getTimesForStop(1010, "Rabb").then(response => console.log(response)).catch(err => console.log("err2: "+err))
 // Query.getNextTime(2010, "Rabb").then(response => console.log(response.toLocaleTimeString())).catch(err => console.log("err2: "+err))
-// Query.getVanScheduleID("campusVan")
+nowExact = new Date(2018, 7, 30)
+date = new Date(Date.UTC(nowExact.getFullYear(), nowExact.getMonth(), nowExact.getDate()))
+
+Query.getVanScheduleID("campusVan", date).then(response => console.log(response)).catch(err => console.log("err2: "+err))
 
 
 // viewengine setup
@@ -157,42 +161,6 @@ app.get('/login/authorized',
         }));
 
 app.post('/webhook', PartnersShuttleController.respondToDF);
-
-// console.log(new Date(Date.UTC(2019, 7, 18)+86400000))
-var start = new Date(Date.UTC(2018, 6, 19, 12))
-var end = new Date(Date.UTC(2018, 6, 19, 12))
-console.log("start:   "+start)
-console.log("end:     "+end)
-
-// EnterVanDays.enterVanDays(start, end, [true,true,true,true,true,true,true], 2010, "campusVan")
-
-//The above line creates one VanDay on August 18th 2019 at 8:00AM EDT
-// EnterVanDays.enterVanDays(new Date(2018, 6, 18), new Date(2019, 6, 18), [true,true,true,true,true,true,true], 0, "campusVan")
-// EnterVanDays.enterVanDays(new Date(2018, 6, 18), new Date(2019, 6, 18), [true,true,true,true,true,true,true], 0, "campusShuttle")
-// EnterVanDays.enterVanDays(new Date(2018, 6, 18), new Date(2019, 6, 18), [true,true,true,true,true,true,true], 0, "walthamVan")
-// EnterVanDays.enterVanDays(new Date(2018, 6, 18), new Date(2019, 6, 18), [true,true,true,true,true,true,true], 0, "walthamShuttle")
-// EnterVanDays.enterVanDays(new Date(2018, 6, 18), new Date(2019, 6, 18), [true,true,true,true,true,true,true], 0, "cambridgeShuttle")
-
-// Query.getStopNames(2010)
-// .then(stops => console.log(stops))
-
-// nowExact = new Date()
-// console.log("nowExact.getYear(): "+nowExact.getYear())
-// console.log("nowExact.getMonth(): "+nowExact.getMonth())
-// console.log("nowExact.getDay(): "+nowExact.getDay())
-// console.log("nowExact.getDate(): "+nowExact.getDate())
-// now = new Date(nowExact.getFullYear(), nowExact.getMonth(), nowExact.getDate(), 4)
-// console.log("now: "+now)
-//
-// today = new Date()
-// console.log(new Date(Date.UTC(2000, 0, 1, 0, 45)))
-// console.log(today)
-// Query.getNextTime(1010, "Usdan").then(response => console.log(response)).catch(err => console.log("err2: "+err))
-// Query.getVanScheduleID("campusVan").then(response => console.log("route ID: "+response)).catch(err => console.log("err2: "+err))
-Query.getNextTimeForVan("campusVan", "Rabb").then(response => console.log("next van is at: "+response)).catch(err => console.log("err2: "+err))
-
-// EnterVanDays.enterVanDays(new Date(2018, 7, 25), new Date(2018, 11, 15), [false,true,true,true,true,true,false], 2010, "campusVan")
-// EnterVanDays.enterVanDays(new Date(2018, 7, 25), new Date(2018, 11, 15), [true,false,false,false,false,false,true], 2011, "campusVan")
 
 
 
