ab667fd2474d3a87bd39eb7536fe283bc3d1a28e
Casper L'Esperance-Kerckhoff <casperlesperancekerckhoff@gmail.com>
2018-07-20 15:01:35 -0400

    rendered schedules using promises

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -10,14 +10,13 @@ exports.getSchedule = (req,res) => {
   console.log("in getSchedules")
   var route = req.body.schedule
   console.log("fetching schedule of route: " + route)
-  var stops = Query.getSchedule(route).then(response => console.log(response)).catch(err => console.log("err2: "+err))
-  var times = []
-  for (var i = 0; i < stops.length; i++) {
-    times[i] = Query.getTimesForStop(route, stops[i]).then(response => console.log(response)).catch(err => console.log("err2: "+err))
-  }
-  console.log(times)
-<<<<<<< HEAD
-
-=======
->>>>>>> Jen
+  var stops = Query.getSchedule(route).then(data =>
+    {
+      res.render('schedules', {data:data})
+    })
+  // var times = []
+  // for (var i = 0; i < stops.length; i++) {
+  //   times[i] = Query.getTimesForStop(route, stops[i]).then(response => console.log(response)).catch(err => console.log("err2: "+err))
+  // }
+  // console.log(times)
 }
