769e885acd6e2573fc47cf5ee9b4d9b9663ee457
Ek Kheng Chung <chungek@brandeis.edu>
2018-07-23 13:00:47 -0400

    fixed hamburger

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -9,14 +9,10 @@ exports.renderMain = (req,res) => {
 exports.getSchedule = (req,res, next) => {
   console.log("in getSchedules")
   var route = req.body.schedule
+  console.log(req.body.schedule.name)
   console.log("fetching schedule of route: " + route)
   var stops = Query.getSchedule(route).then(data =>
     {
       res.render('schedules', {data:data})
     })
-  // var times = []
-  // for (var i = 0; i < stops.length; i++) {
-  //   times[i] = Query.getTimesForStop(route, stops[i]).then(response => console.log(response)).catch(err => console.log("err2: "+err))
-  // }
-  // console.log(times)
 }
