747d113f10b3ad6f35f9f845f657765486ab565a
jenn14108 <jenn.lee14108@gmail.com>
2018-07-25 22:55:07 -0400

    Get geolocation working for coordinates

--- public/javascripts/locateUser.js
+++ public/javascripts/locateUser.js
@@ -1,7 +1,11 @@
+var lat;
+var long;
+var map;
 $( document ).ready(function() {
   $("button[name='locate-button']").on('click',function() {
        console.log('button clicked');
        getLocation();
+       // initMap();
   });
 });
 
@@ -9,9 +13,23 @@ function getLocation() {
   if (navigator.geolocation) {
     console.log("geolocation working!");
     navigator.geolocation.getCurrentPosition(function(position){
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      console.log(latitude, longitude);
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      console.log(lat, long);
     });
   }
 }
+
+// function initMap() {
+//   var myLatlng = new google.maps.LatLng(42.3756803, -71.2375709);
+//   var mapOptions = {
+//     zoom: 4,
+//     center: myLatlng
+//   }
+//   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//   var marker = new google.maps.Marker({
+//       position: myLatlng,
+//   });
+//   marker.setMap(map);
+//   console.log("done")
+// }
