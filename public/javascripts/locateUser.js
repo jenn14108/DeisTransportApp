bed939f5ebd3610447311b998b0aee8bd77f5d14
jenn14108 <jenn.lee14108@gmail.com>
2018-07-25 21:11:51 -0400

    Change jquery setting to only fire off geolocation when button is clicked

--- public/javascripts/locateUser.js
+++ public/javascripts/locateUser.js
@@ -1,15 +1,17 @@
-$(document).ready("locate-button").on('click',function() {
+$( document ).ready(function() {
+  $("button[name='locate-button']").on('click',function() {
        console.log('button clicked');
        getLocation();
+  });
 });
 
-    function getLocation() {
-      if (navigator.geolocation) {
-        console.log("geolocation working!");
-        navigator.geolocation.getCurrentPosition(function(position){
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          console.log(latitude, longitude);
-        });
-      }
-    }
+function getLocation() {
+  if (navigator.geolocation) {
+    console.log("geolocation working!");
+    navigator.geolocation.getCurrentPosition(function(position){
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      console.log(latitude, longitude);
+    });
+  }
+}
