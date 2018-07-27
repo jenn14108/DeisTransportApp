c8953149101b3f0f45d7ea4b8bdccf86924516f2
jenn14108 <jenn.lee14108@gmail.com>
2018-07-27 15:42:31 -0400

    Prevent incomplete forms from being saved in db

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -28,35 +28,43 @@ exports.getRouteInfo = (req,res) => {
 }
 
 exports.addReservation = (req,res) => {
-  console.log("in addReservation")
-  var routeNum = req.body.vanType
-  var route;
-  //determine which van
-  if (routeNum == "2010") {
-    route = "Campus BranVan (Weekdays)";
-  } else if (routeNum == "2011") {
-    route = "Campus BranVan (Weekends)";
-  } else if (routeNum == "3010") {
-    route = "Evening Waltham Branvan";
-  }
-  var todayDate = moment().format('LL')
-  //start creating a new reservation
-  console.log("creating a new reservation...");
-  let newReservation = new reservation({
-    van_name : route,
-    from: req.body.stopFrom,
-    to: req.body.stopTo,
-    pickup_time: req.body.time,
-    date: todayDate,
-    num_people: req.body.numPeople
-  });
-  newReservation.save()
-    .then( () => {
-      res.render('reserve');
-    })
-    .catch( error => {
-      res.send(error);
+  if ((req.body.vanType === 'Select a van')
+       || (req.body.stopFrom === 'Select a stop')
+       || (req.body.stopTo === 'Select a stop')
+       || (req.body.time === 'Select a time')
+       || (req.body.numPeople === 'Select the number of people')){
+         console.log("Incomplete Reservation. Not saved.");
+  } else {
+    console.log("in addReservation")
+    var routeNum = req.body.vanType
+    var route;
+    //determine which van
+    if (routeNum == "2010") {
+      route = "Campus BranVan (Weekdays)";
+    } else if (routeNum == "2011") {
+      route = "Campus BranVan (Weekends)";
+    } else if (routeNum == "3010") {
+      route = "Evening Waltham Branvan";
+    }
+    var todayDate = moment().format('LL')
+    //start creating a new reservation
+    console.log("creating a new reservation...");
+    let newReservation = new reservation({
+      van_name : route,
+      from: req.body.stopFrom,
+      to: req.body.stopTo,
+      pickup_time: req.body.time,
+      date: todayDate,
+      num_people: req.body.numPeople
     });
+    newReservation.save()
+      .then( () => {
+        res.render('reserve');
+      })
+      .catch( error => {
+        res.send(error);
+      });
+  }
 };
 
 
