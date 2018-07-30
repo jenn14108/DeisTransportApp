e4fb82399c069d084499c8861064b5880c9fa7b6
jenn14108 <jenn.lee14108@gmail.com>
2018-07-30 15:48:32 -0400

    Merge changes

--- controllers/DFShuttleController.js
+++ controllers/DFShuttleController.js
@@ -190,9 +190,9 @@ exports.respondToDF = (req, res) => {
             case "getArrivalEstimate":
             Session.findOne({session : session_id} , function (err, session_obj) {
               if (err){
-                return res.json("Sorry, I could not find the arrival time of the next shuttle.")
+                return res.json({"fulfillmentText":"Sorry, I could not find the arrival time of the next shuttle."})
               } else if (!session_obj) {
-                return res.json("Sorry, something went wrong with your login.")
+                return res.json({"fulfillmentText":"Sorry, something went wrong with your login."})
               } else {
                 console.log("route: "+route)
                 console.log("stop: "+stop)
@@ -221,7 +221,7 @@ exports.respondToDF = (req, res) => {
               if (err){
                 return res.json("Sorry, I could not find whether the "+route+" is running.")
               } else if (!session_obj) {
-                return res.json("Sorry, something went wrong with your login.")
+                return res.json({"fulfillmentText":"Sorry, something went wrong with your login."})
               } else {
                 if (!date) {
                   date = new Date()
@@ -284,31 +284,54 @@ exports.respondToDF = (req, res) => {
             console.log("in makeReservation")
             Session.findOne({session : session_id} , function (err, session_obj) {
               if (err){
-                return res.json("Sorry, I could not find whether the "+route+" is running.")
+                return res.json({"fulfillmentText":"Sorry, I could not make a reservation."})
               } else if (!session_obj) {
-                return res.json("Sorry, something went wrong with your login.")
+                return res.json({"fulfillmentText":"Sorry, something went wrong with your login."})
               } else {
+                if (brandeisUsername == "temp") {
+                  return res.json({"fulfillmentText":"Looks like you aren't logged in. Please log in with your Brandeis Email."})
+                } else {
 
-                let newReservation = new reservation({
-                  name: brandeisUsername,
-                  van_name : route,
-                  from: stop,
-                  to: stop2,
-                  pickup_time: time,
-                  date: new Date(),
-                  num_people: numSeats
-                });
+                  // var todayExact = new Date()
+                  // time.setDate(todayExact.getDate())
+                  // time.setMonth(todayExact.getMonth())
 
-                newReservation.save()
-                  .then(error => {
-                    setTimeout(function(){
-                      var string = "I made you a reservation from "+stop+" to "+stop2+" on the "+route+" for "+numSeats+ " people at "+time
-                      return res.json({"fulfillmentText":string})
-                    }, 2000)
-                  })
-                  .catch( error => {
-                    return res.json({"fulfillmentText":"I wasn't able to reserve for that time."})
+                  if (route == "walthamVan") {route = "Evening Waltham Branvan"}
+                  if (route == "campusVan") {route = "Campus BranVan (Weekdays)"}
+                  if (route == "campusVan" && (time.getDay()==0 || time.getDay()==6)) {route = "Campus BranVan (Weekends)"}
+
+                  var momentTime = moment(time)
+
+
+                  var todate = moment().format('LL')
+                  console.log("momentTime: "+momentTime.format('LT'))
+                  let newReservation = new reservation({
+                    name: brandeisUsername,
+                    van_name : route,
+                    from: stop,
+                    to: stop2,
+                    pickup_time: momentTime.format('LT'),
+                    date: todate,
+                    num_people: numSeats
                   });
+
+                  var minutes = time.getMinutes()
+                  if (minutes == 0) {minutes = "00"}
+                  var people = "people"
+                  if (numSeats == 1) people = "person"
+
+                  newReservation.save()
+                    .then(error => {
+                      setTimeout(function(){
+                        var string = "I made you a reservation from "+stop+" to "+stop2+" on the "+route+" for "+numSeats+" "+people+ " on the "+time.getHours()+":"+minutes+" run, "+todate
+                        return res.json({"fulfillmentText":string})
+                      }, 2000)
+                    })
+                    .catch( error => {
+                      return res.json({"fulfillmentText":"I wasn't able to reserve for that time."})
+                    });
+
+                }
 
               }
             });
--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -7,13 +7,65 @@ exports.renderMain = (req,res) => {
   res.render('tracker', {title: "Tracker"});
 };
 
+exports.dispRoute = (req,res) => {
+  console.log("in dispRoute")
+  var route = req.body.vanType
+  var name;
+  if (route == "2010") {
+    name = "Campus BranVan (Weekdays)"
+  } else if (route == "2011") {
+    name = "Campus BranVan (Weekends)"
+  } else if (route == "2020") {
+    name = "Daytime Campus Shuttle"
+  } else if (route == "3020") {
+    name = "Daytime Waltham Shuttle"
+  } else if (route == "3010") {
+    name = "Evening Waltham Branvan"
+  } else if (route == "1010") {
+    name = "Boston/Cambridge Shuttle (Thurs)"
+  } else if (route == "1020") {
+    name = "Boston/Cambridge Shuttle (Fri)"
+  } else if ( route == "1030") {
+    name = "Boston/Cambridge Shuttle (Sat)"
+  } else if (route == "1040") {
+    name = "Boston/Cambridge Shuttle (Sun)"
+  }
+  console.log("fetching info of route: " + name)
+  var stops = Query.getSchedule(route).then(data =>
+    {
+      //res.render('schedules', {name:name, data:data})
+      res.json({data: data});
+    })
+}
+
 exports.getEstimate = ( req, res) => {
   console.log("in getEstimate")
   const partnersQuery = new transLocAPI(707);
   const response = {};
   //construct the two needed query parameters for API calls
-  const route = req.body.route.replace("-","–");;
-  const stop = (req.body.stop).replace("–","-");
+  const
+    route = req.body.vanType
+    stop = req.body.stop
+    shortname
+  //campus bvan
+  if (route == 2011 || route == 2010) {
+    shortname = "Yellow"
+  //waltham bvan & extra waltham
+  } else if (route == 3010) {
+    shortname = "Blue"
+    const shortname2 = "Green"
+  //campus josephs
+  } else if (route == 2020) {
+    shortname = "Red"
+  //waltham josephs
+  } else if (route == 3020) {
+    shortname = "Orange"
+  //boston shuttles
+  } else if (route == 1010 || route == 1020 || route == 1030 || route == 1040) {
+    shortname = "White"
+    const shortname2 = "Purple"
+    const shortname3 = "Pink"
+  }
   console.log("fetching ETA of next van at " + stop + " on the " + route + " route.")
   //construct two variables needed later
   var route_id = "";
@@ -56,3 +108,54 @@ exports.getEstimate = ( req, res) => {
     }
   });
 };
+
+
+// exports.getEstimate = ( req, res) => {
+//   console.log("in getEstimate")
+//   const partnersQuery = new transLocAPI(707);
+//   const response = {};
+//   //construct the two needed query parameters for API calls
+//   const route = req.body.route.replace("-","–");;
+//   const stop = (req.body.stop).replace("–","-");
+//   console.log("fetching ETA of next van at " + stop + " on the " + route + " route.")
+//   //construct two variables needed later
+//   var route_id = "";
+//   var stop_id = "";
+//   var arrival_times = [];
+//
+//   async.waterfall([
+//     function(callback){
+//       partnersQuery.findRouteId(route, callback);
+//     },
+//     function(route_id, callback){
+//       partnersQuery.findStopId(stop, function(err, result){
+//         if(err){
+//           callback(err, null);
+//         } else {
+//           callback(null, route_id, result);
+//         }
+//       });
+//     },
+//     function(route_id, stop_id, callback) {
+//       partnersQuery.findArrivalEstimate(route_id, stop_id, callback);
+//     }
+//   ],
+//   function(err, result){
+//     if(err){
+//       console.log("Sorry, I could not retrieve any information");
+//     } else {
+//       if (typeof result[0] === 'undefined' ){
+//         res.render('tracker', {route:route, stop:stop, arrival_time1: "No arrivals"});
+//       } else {
+//         if (typeof result[1] === 'undefined'){
+//           res.render('tracker', {route:route, stop:stop,
+//                                 arrival_time1:result[0].substring(11,16)});
+//         } else {
+//           res.render('tracker', {route:route, stop:stop,
+//                                 arrival_time1:result[0].substring(11,16),
+//                                 arrival_time2:result[1].substring(11,16)});
+//         }
+//       }
+//     }
+//   });
+// };
